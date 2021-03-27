f823cd9fafae28552c33c34808e86021ee6114ef
Evgenii Baleevskii <evgenii.baleevskii@helmes.ee>
2021-03-27 14:55:41 +0200

    Add Base64Utility

---
+++ Aiirh.Basic/Utilities/Base64Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Aiirh.Basic.Utilities
+{
+    public static class Base64Utility
+    {
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Encode<T>(this T obj)
+        {
+            var json = obj.Convert();
+            var plainTextBytes = Encoding.UTF8.GetBytes(json);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static T Base64Decode<T>(this string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            var json = Encoding.UTF8.GetString(base64EncodedBytes);
+            return json.Convert<T>();
+        }
+    }
+}

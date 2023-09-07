079a8c1148dc64c8c777c4ac4118e23da6f318a2
Evgenii Baleevskii <evgenii.baleevskii@helmes.com>
2023-09-07 11:40:37 +0300

    Create revision creator

---
+++ Aiirh.Basic/Audit/RevisionCreator.cs
@@ -0,0 +1,54 @@
+﻿using Aiirh.Basic.Utilities;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aiirh.Basic.Audit
+{
+    internal static class RevisionCreator
+    {
+        public static string CreateRevision<T>(T obj)
+        {
+            var type = typeof(T);
+            var attributes = type.GetPropertyAttributes<AuditableAttribute>();
+            if (attributes.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            return JsonConvert.SerializeObject(obj, new JsonSerializerSettings { ContractResolver = new RevisionsContractResolver(attributes) });
+        }
+    }
+
+    public class RevisionsContractResolver : DefaultContractResolver
+    {
+        private readonly Dictionary<string, string> _attributesToCreate;
+
+        public RevisionsContractResolver(Dictionary<PropertyInfo, AuditableAttribute> attributes)
+        {
+            _attributesToCreate = attributes.ToDictionary(x => x.Key.Name, x => x.Value.PropertyName);
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            if (string.IsNullOrWhiteSpace(property.PropertyName))
+            {
+                property.ShouldSerialize = _ => false;
+                return property;
+            }
+
+            if (!_attributesToCreate.TryGetValue(property.PropertyName, out var newName))
+            {
+                property.ShouldSerialize = _ => false;
+                return property;
+            }
+
+            property.PropertyName = newName;
+            return property;
+        }
+    }
+}
---
+++ Aiirh.CommonLibraries.Tests/Audit/RevisionCreatorTests.cs
@@ -0,0 +1,78 @@
+﻿using Aiirh.Basic.Audit;
+using NUnit.Framework;
+
+namespace Aiirh.CommonLibraries.Tests.Audit
+{
+    [TestFixture]
+    public class RevisionCreatorTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            var data = new MyAuditTestParent
+            {
+                Property1 = "Test1",
+                Property2 = "Test2",
+                Property3 = "Test3",
+                Property4 = null,
+                ListIgnored = new[] { "123", "456" },
+                ListIncluded = new[] { "789", "ABC" },
+                ComplexListIncluded = new[]
+                {
+                    new MyAuditTestChild
+                    {
+                        Property1 = "C1",
+                        Property2 = "C2",
+                        Property3 = "C3"
+                    },
+                    new MyAuditTestChild
+                    {
+                        Property1 = "C4",
+                        Property2 = "C5",
+                        Property3 = "C6"
+                    }
+                }
+            };
+            var result = RevisionCreator.CreateRevision(data);
+        }
+
+        public class MyAuditTestParent
+        {
+            public string Property1 { get; set; }
+
+            [Auditable("Name")]
+            public string Property2 { get; set; }
+
+            [Auditable("Description")]
+            public string Property3 { get; set; }
+
+            [Auditable("NullToBeHere")]
+            public string Property4 { get; set; }
+
+            public string[] ListIgnored { get; set; }
+
+            [Auditable("SimpleCollection")]
+            public string[] ListIncluded { get; set; }
+
+            [Auditable("ComplexCollection")]
+            public MyAuditTestChild[] ComplexListIncluded { get; set; }
+        }
+
+        public class MyAuditTestChild
+        {
+            public string Property1 { get; set; }
+
+            [Auditable("ChildName")]
+            public string Property2 { get; set; }
+
+            [Auditable("ChildDescription")]
+            public string Property3 { get; set; }
+        }
+    }
+}
